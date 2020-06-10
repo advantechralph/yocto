65973a6c61261a93c4cd79af0705580b6209b225
Ralph Wang <advantechralph@gmail.com>
2020-06-10 16:19:26 +0800

    update makefiles

---
+++ include/usage.mk
@@ -0,0 +1,43 @@
+define usage_str
+
+##########################################
+# distro=$(yoctodistro)                  
+# machine=$(yoctomachine)                
+##########################################
+
+Usage: 
+	make all          - Set up yocto layers and build fsl-image-qt5. 
+	make prepare      - Set up yocto layers. 
+	make build        - Build core-image-minimal. 
+	make buildqt5     - Build fsl-image-qt5
+	make u-boot-imx   - Build u-boot-imx
+	make linux-imx    - Build linux-imx
+
+  --------
+
+  Yoc can do Yocto Project by manual with commands as below: 
+
+    $$ make prepare
+    $$ cd build/yocto/build.$(yoctomachine)
+    $$ source setup-environment build.$(yoctomachine)
+
+  Then, you can run 'bitbake' commands. 
+  ex: 
+    $$ bitbake u-boot-imx 
+
+  --------
+
+  If you want to change the working directory for downloads, sstate_cache 
+  and tmp folder location, please define 'YOCTO_WORK_DIR'. 
+  ex: 
+    $$ YOCTO_WORK_DIR=/root/imx-linux-warrior make prepare
+
+  Currently, YOCTO_WORK_DIR=$(shell if [ -f "$(yoctodir)/$(yoctobuilddir)/conf/extra-local.conf" ] ; then source $(yoctodir)/$(yoctobuilddir)/conf/extra-local.conf; echo $$DL_DIR| sed -e 's/\/downloads$$//'; fi)
+
+endef
+export usage_str
+
+usage help: 
+	@echo "$${usage_str}"
+
+default: usage; 
