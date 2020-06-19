b797e6663afc9fa65492bba476a3884aefa17dfc
Ralph Wang <advantechralph@gmail.com>
2020-06-19 17:37:49 +0800

    update makefiles for write image

--- include/info.mk
+++ include/info.mk
@@ -10,6 +10,7 @@ ifneq ($(YOCTO_WORK_DIR_CHK),)
 endif
 	@printf "%-20s: %s\n" "download folder" "$(yoctoworkdir)/downloads"
 	@printf "%-20s: %s\n" "deploy folder" "$(yoctoworkdir)/tmp/deploy"
+	@printf "%-20s: %s\n" "sdk folder" "$(yoctoworkdir)/tmp/sdk"
 	@printf "%-20s: %s\n" "images folder" "$(yoctoworkdir)/tmp/deploy/images/$(yoctomachine)"
 	@printf "%-20s: %s\n" "fsl-image-qt5" "$(fslimageqt5)"
 
